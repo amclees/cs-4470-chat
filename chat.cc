d1be2ee29ac540a3f288a829958e571226db2605
amclees <andrewmclees5@gmail.com>
2017-09-15 17:05:05 -0700

    Add skeleton file

---
+++ chat.cc
@@ -0,0 +1,19 @@
+void listen_new_connections(int port) {
+
+}
+
+void listen_messages(/*socket*/) {
+
+}
+
+void connect(char* dest, int port) {
+
+}
+
+void handle_cin() {
+
+}
+
+int main(int argc, char** argv) {
+  return 0;
+}
